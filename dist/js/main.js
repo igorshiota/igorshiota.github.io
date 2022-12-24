5b3baef736d3f174031ea54dc6dfc8959a0f7125
Igor <igor.shiota@gmail.com>
2022-12-24 19:27:27 -0300

    contact modal sorted

--- dist/js/main.js
+++ dist/js/main.js
@@ -58,4 +58,35 @@ $(document).keydown(function(event) {
   });
 
 
+
+//////////////////////////
+
+//Contact Modal
+
+// Get the modal
+var ebModal = document.getElementById('contact-modal');
+
+// Get the button that opens the modal
+var ebBtn = document.getElementById("contact-modal-link");
+
+// Get the <span> element that closes the modal
+var ebSpan = document.getElementsByClassName("ebcf_close")[0];
+
+// When the user clicks the button, open the modal 
+ebBtn.onclick = function() {
+    ebModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+ebSpan.onclick = function() {
+    ebModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == ebModal) {
+        ebModal.style.display = "none";
+    }
+}
+
   
